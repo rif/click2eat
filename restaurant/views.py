ca441e79ea52b4fb002d303cdb52e29cc0112abf
Radu Ioan Fericean <fericean@gmail.com>
2010-09-15 19:08:23 +0300

    link to the admin for first view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello bucatar")
+    return HttpResponse('Hello bucatar, please head to <a href="%s">admin section</a>' % reverse("admin:index"))
