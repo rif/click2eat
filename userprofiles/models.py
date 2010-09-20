c8708cd6d14a56ab11c85605f04dbaf36eebab43
Radu Ioan Fericean <fericean@gmail.com>
2010-09-20 18:37:09 +0300

    various changes

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -3,26 +3,30 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 class DeliveryAddress(models.Model):
-  primary = models.BooleanField()
-  city = models.CharField(max_length=50)
-  street = models.CharField(max_length=50)
-  house_number = models.CharField(max_length=5)
-  street_number = models.CharField(max_length=5,null=True,blank=True)
-  floor = models.SmallIntegerField()
-  ap_number = models.SmallIntegerField()
-  additional_info = models.TextField(null=True, blank=True)
+  primary = models.BooleanField(_('primary'))
+  city = models.CharField(_('city'), max_length=50)
+  street = models.CharField(_('street'), max_length=50)
+  house_number = models.CharField(_('house number'), max_length=5)
+  street_number = models.CharField(_('street number'), max_length=5,null=True,blank=True)
+  floor = models.SmallIntegerField(_('floor'), )
+  ap_number = models.SmallIntegerField(_('apartment number'), )
+  additional_info = models.TextField(_('additional info'), null=True, blank=True)
+  
+  class Meta:
+      verbose_name = _('Delivery Address')
+      verbose_name_plural = _('Delivery Addresses')
 
 class UserProfile(models.Model):
   GENDER_CHOICES = (
       ('M', 'Male'),
       ('F', 'Female'),
   )
-  user = models.ForeignKey(User, unique=True)
-  phone = models.CharField(max_length=15)
-  newsletter = models.BooleanField(help_text="Do you want to receive our newsletter?")
-  sex = models.CharField(max_length=1, choices=GENDER_CHOICES)
-  birth_date = models.DateField()
-  delivery_address = models.ForeignKey('DeliveryAddress')
+  user = models.ForeignKey(User, unique=True, verbose_name=_('user'))
+  phone = models.CharField(_('phone'), max_length=15)
+  newsletter = models.BooleanField(_('newsletter'), help_text=_("Do you want to receive our newsletter?"))
+  sex = models.CharField(_('sex'), max_length=1, choices=GENDER_CHOICES)
+  birth_date = models.DateField(_('birth date'))
+  delivery_address = models.ForeignKey('DeliveryAddress', verbose_name=_('delivery address'))
 
   def is_filled(self):
       return self.phone != '' or self.sex != '' or self.birthday != ''
@@ -30,4 +34,8 @@ class UserProfile(models.Model):
   @models.permalink
   def get_absolute_url(self):
         return ('profiles_profile_detail', (), { 'username': self.user.username })
+  
+  class Meta:
+      verbose_name = _('User Profile')
+      verbose_name_plural = _('User Profiles')
     
