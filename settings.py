d8b3028326096cd8b7b187d18369d44d913c5c20
Radu Ioan Fericean <fericean@gmail.com>
2010-09-20 14:11:42 +0300

    registration and profiles for testing

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import socket
-if socket.gethostname() in ('grace', 'love', 'cocostar', 'old-mac'):
+if socket.gethostname() in ('grace', 'love', 'cocostar', 'old-mac', 'Virgils-Mac-Pro-4.local'):
     from settings_dev import *
 else:
     from settings_prod import *
