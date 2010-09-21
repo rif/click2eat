d29ea7face9ecb482c7864325576ad44f3e2d476
Radu Ioan Fericean <radu@fericean.ro>
2010-09-21 18:07:17 +0300

    added timer js

--- restaurant/views.py
+++ restaurant/views.py
@@ -14,11 +14,12 @@ def index(request):
         if not user.is_authenticated() or user.get_profile().is_filled():
             return render_to_response('restaurant/index.html', {
                                                                 'units': units,
-                                                                'gold': units.filter(package__slug='gold')[0],
-                                                                'platinum': units.filter(package__slug='platinum')[0],
+                                                                'gold': units.filter(package__slug='gold'),
+                                                                'platinum': units.filter(package__slug='platinum'),
                                                                 }, context_instance=RequestContext(request))
         else:
             return redirect('profiles_create_profile')
     except User.DoesNotExist:
         return redirect('profiles_create_profile')
 
+    
