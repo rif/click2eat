9919aa188b846f683332633ba1acb06f0985745e
Radu Ioan Fericean <radu@fericean.ro>
2010-09-21 12:30:51 +0300

    added uniform

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,9 +3,15 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def index(request):
-    if request.user.is_authenticated():
-        return HttpResponse('Hello %s, you can logout <a href="%s">here</a>' % (request.user, "/accounts/logout/"))
-    else:
-        return HttpResponse('Hello %s, please <a href="%s">login</a>' % (request.user, "/accounts/login/"))
+    try:
+        if request.user.get_profile() != None:
+            return render_to_response('restaurant/index.html', context_instance=RequestContext(request))
+        else:
+            return redirect('profiles_create_profile')
+    except:
+        return redirect('profiles_create_profile')
+
