9a5043b419c579269d05c503f25ace4be7afc511
Radu Ioan Fericean <radu@fericean.ro>
2010-09-23 15:01:24 +0300

    order list and detail

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import list_detail
 from order.models import Order
 
 urlpatterns = patterns('',
-                       url(r'^$list/', views.list, name='list'),
+                       url(r'^list/$', views.list, name='list'),
                        url(r'^(?P<object_id>\d+)/$', list_detail.object_detail, {'queryset': Order.objects.all()}, name='detail'),
+                       url(r'^create/(?P<unit_id>\d+)/$', views.create, name='create'),
                        )
--- order/views.py
+++ order/views.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponse
+from order.models import Order
+from restaurant.models import Unit
 
 
 @login_required
 def list(request):
     orders = Order.objects.filter(user__id=request.user.id)
     return render_to_response('order/order_list.html', {
-                                  'orders': orders,
+                                  'order_list': orders,
                                   }, context_instance=RequestContext(request))
+    
+def create(request, unit_id):
+    unit = get_object_or_404(Unit, pk=unit_id)
+    order = Order.objects.create(user=request.user, unit=unit)
+    return HttpResponse('OK')
