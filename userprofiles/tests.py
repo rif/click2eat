b19e94a4d4c1ad44f99445274e6c282c23bdd437
Radu Ioan Fericean <fericean@gmail.com>
2010-09-23 11:51:43 +0300

    tests for primary address uniqueness

--- userprofiles/tests.py
+++ userprofiles/tests.py
@@ -1,23 +1,18 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from userprofiles.models import DeliveryAddress
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+class DeliveryAddressTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('rif', 'test@test.te', 'test')
+        self.da1 = DeliveryAddress.objects.create(user=self.user,primary=True,city='1',street='1',house_number='1',floor=1,ap_number=1)
+        
+    def test_primary_uniquenes(self):
+        das = DeliveryAddress.objects.filter(user__id=self.user.id).filter(primary=True)
+        self.failUnlessEqual(das.count(), 1)
+        da2 = DeliveryAddress.objects.create(user=self.user,primary=True,city='1',street='1',house_number='1',floor=1,ap_number=1)
+        all_count = DeliveryAddress.objects.count()
+        self.failUnlessEqual(all_count, 2)
+        das = DeliveryAddress.objects.filter(user__id=self.user.id).filter(primary=True)
+        # still one primary address
+        self.failUnlessEqual(das.count(), 1)
