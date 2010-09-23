f978f309cf7f1fd391fde9ff3f2918f44db4c88b
Radu Ioan Fericean <fericean@gmail.com>
2010-09-23 13:21:19 +0300

    going to lunch

---
+++ order/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+from order import views
+from django.views.generic import list_detail
+from order.models import Order
+
+urlpatterns = patterns('',
+                       url(r'^$list/', views.list, name='list'),
+                       url(r'^(?P<object_id>\d+)/$', list_detail.object_detail, {'queryset': Order.objects.all()}, name='detail'),
+                       )
