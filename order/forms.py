2b6a7a08de03d6c0f880c8ec270eb9b1063c6b25
Radu Ioan Fericean <fericean@gmail.com>
2010-09-28 16:42:07 +0300

    additional info for order

--- order/forms.py
+++ order/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from order.models import Order
 
 class CartNameForm(forms.Form):
     name = forms.CharField(max_length=10)
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('additional_info',)
