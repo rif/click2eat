62b77aad45d16d0aa8ef845df7c0b03931e16500
Radu Ioan Fericean <radu@fericean.ro>
2010-09-29 18:21:38 +0300

    third try

--- order/forms.py
+++ order/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-#from django.db.models import F
+from django.db.models import F
 from order.models import Order
 from userprofiles.models import DeliveryAddress
 
