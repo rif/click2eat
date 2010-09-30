132ea6abc9424ae951451a3e566c9f449c1ee0c1
Radu Ioan Fericean <fericean@gmail.com>
2010-09-30 12:32:16 +0300

    currency and admin improvements for Unit

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -21,6 +21,10 @@ class DeliveryTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
     search_fields = ['name']
 
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
 class ItemInline(admin.TabularInline):
    model = Item
    extra = 0
@@ -35,6 +39,24 @@ class UnitAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'package', 'email', 'phone')
     search_fields = ['name', 'address']
     inlines = [ItemGroupInline, ItemInline]
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'address', 'email', 'phone', 'logo_path', 'package', 'open_hours')
+        }),
+        ('Location options', {
+            'fields': ('unit_devlivery', 'latitude', 'longitude', 'delivery_type', 'delivery_time', 'delivery_time_user')
+        }),
+        ('Administration', {
+            'fields': ('contact_person', 'admin_users', 'employee', 'communication')
+        }),
+        ('Payment info', {
+            'fields': ('payment_method', 'currency', 'overall_discount', 'minimum_ord_val')
+        }),
+        ('Other info', {
+            'classes': ('collapse',),
+            'fields': ('info', 'active')
+        }),
+    )
     
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'email', 'phone', 'start_date', 'end_date')
@@ -51,6 +73,5 @@ admin.site.register(models.DeliveryType, DeliveryTypeAdmin)
 admin.site.register(models.PaymentMethod,PaymentMethodAdmin)
 admin.site.register(models.Communication, CommunicationAdmin)
 admin.site.register(models.PartnerPackage, PartnerPackageAdmin)
-admin.site.register(models.DeliveryArea, DeliveryAreaAdmin )
-
-
+admin.site.register(models.DeliveryArea, DeliveryAreaAdmin)
+admin.site.register(models.Currency, CurrencyAdmin)
