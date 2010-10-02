2b35ad4b67d72c4f92a57170d208194b239c0988
Radu Fericean <radu.fericean@oce.com>
2010-10-02 14:18:43 +0300

    code cleaning

--- order/forms.py
+++ order/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
-from django.db.models import F
 from order.models import Order
-from userprofiles.models import DeliveryAddress
 
 class CartNameForm(forms.Form):
     name = forms.CharField(max_length=10)
