aa03a62b5b881486091c6012857430de7e803c9e
Radu Ioan Fericean <radu@fericean.ro>
2010-10-03 13:54:36 +0300

    used django annoying

--- urls.py
+++ urls.py
@@ -13,13 +13,6 @@ urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
 
-# if we're in DEBUG mode, allow django to serve media
-# This is considered inefficient and isn't secure.
-from django.conf import settings
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
