96c5955d0bea8003ef30472ed52dd73fe7a5652d
Radu Ioan Fericean <rif@love.ro>
2010-10-06 22:26:36 +0300

    spelling error

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -14,7 +14,7 @@ def create(request):
             new_address = form.save(commit=False)
             new_address.user = request.user
             new_address.save()
-            return redirect('userprofiles:address_detail', object_id=new_addres.id)
+            return redirect('userprofiles:address_detail', object_id=new_address.id)
     else:
         form = DeliveryAddressForm()
 
