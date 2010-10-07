a46e297fbf78b79a17e8c23aed4595095dcd01b4
Radu Ioan Fericean <radu@fericean.ro>
2010-10-07 15:56:07 +0300

    error and verification for geolocation

--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -3,6 +3,7 @@ from userprofiles import models
 
 class DeliveryAddressAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'user', 'additional_info', 'geolocated_address', 'primary')
+    list_filter = ['geolocation_error', 'verified']
     search_fields = ['user__first_name', 'user__last_name', 'street', 'additional_info', 'geolocated_address']
 
 class UserProfileAdmin(admin.ModelAdmin):
