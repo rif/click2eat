e5104098c3d813f35b4aa29201811cec6b512911
Radu Ioan Fericean <radu@fericean.ro>
2010-10-08 14:10:20 +0300

    geolocation error

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
                        url(r'^createaddress/$', views.create, name='create_address'),
                        url(r'^updateaddress/(?P<object_id>\d+)/$', views.limited_update_object, {'form_class': DeliveryAddressForm, 'login_required': True}, name='update_address'),
                        url(r'^deleteaddress/(?P<object_id>\d+)/$', views.limited_delete_object, {'model': DeliveryAddress, 'post_delete_redirect': reverse('restaurant:index'), 'login_required': True}, name='delete_address'),
-                       url(r'^addrdetail/(?P<object_id>\d+)/$$', views.limited_object_detail, {'queryset': DeliveryAddress.objects.all()}, name='address_detail'),
+                       url(r'^addrdetail/(?P<object_id>\d+)/$', views.limited_object_detail, {'queryset': DeliveryAddress.objects.all()}, name='address_detail'),
+                       url(r'^geoerror/(?P<object_id>\d+)/$', views.mark_geolocation_error, name='geolocation_error'),
                        )
 
