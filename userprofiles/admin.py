32df1924a078071adc5b8db5c3bc4ab87a658dc1
Radu Ioan Fericean <radu@fericean.ro>
2010-10-08 11:15:52 +0300

    removed verified field and added colne item action

--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from userprofiles import models
 
 class DeliveryAddressAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'user', 'additional_info', 'geolocated_address', 'primary')
-    list_filter = ['geolocation_error', 'verified']
+    list_display = ('__unicode__', 'user', 'additional_info', 'geolocated_address', 'latitude', 'longitude', 'geolocation_error', 'primary')
+    list_filter = ['geolocation_error']
     search_fields = ['user__first_name', 'user__last_name', 'street', 'additional_info', 'geolocated_address']
 
 class UserProfileAdmin(admin.ModelAdmin):
