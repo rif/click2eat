53d423dc756693279bde4c1d79ab8108b3f19812
Radu Ioan Fericean <radu@fericean.ro>
2010-10-09 13:16:24 +0300

    created form for inviting friend

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -5,3 +5,6 @@ class DeliveryAddressForm(forms.ModelForm):
     class Meta:
         model = DeliveryAddress
         exclude = ('user', 'geolocated_address', 'latitude', 'longitude', 'geolocation_error')
+        
+class InviteFriendForm(forms.Form):
+    email = forms.EmailField()
