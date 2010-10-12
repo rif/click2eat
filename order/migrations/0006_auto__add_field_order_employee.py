3781f8e662b8be60b3c11522181fd0ae5bba947c
Radu Ioan Fericean <radu@fericean.ro>
2010-10-12 16:37:31 +0300

    employee field in order

---
+++ order/migrations/0006_auto__add_field_order_employee.py
@@ -0,0 +1,213 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Order.employee'
+        db.add_column('order_order', 'employee', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['restaurant.Employee']), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Order.employee'
+        db.delete_column('order_order', 'employee_id')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'menu.item': {
+            'Meta': {'ordering': "['index']", 'object_name': 'Item'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'internal_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'item_group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['menu.ItemGroup']", 'null': 'True', 'blank': 'True'}),
+            'measurement_unit': ('django.db.models.fields.CharField', [], {'default': "'GR'", 'max_length': '2'}),
+            'new_item_end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'price': ('django.db.models.fields.FloatField', [], {}),
+            'promotion': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['menu.Promotion']", 'null': 'True', 'blank': 'True'}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {}),
+            'toppings': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['menu.ToppingGroup']", 'null': 'True', 'blank': 'True'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Unit']"}),
+            'vat': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['menu.VAT']"})
+        },
+        'menu.itemgroup': {
+            'Meta': {'ordering': "['index']", 'object_name': 'ItemGroup'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'exclusive': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'internal_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Unit']"})
+        },
+        'menu.promotion': {
+            'Meta': {'ordering': "['-start_date']", 'object_name': 'Promotion'},
+            'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'end_hour': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'start_hour': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Unit']"}),
+            'value': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'weekdays': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '13', 'null': 'True', 'blank': 'True'})
+        },
+        'menu.toppinggroup': {
+            'Meta': {'object_name': 'ToppingGroup'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'internal_name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'menu.vat': {
+            'Meta': {'object_name': 'VAT'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'value': ('django.db.models.fields.FloatField', [], {})
+        },
+        'order.order': {
+            'Meta': {'ordering': "['-creation_date']", 'object_name': 'Order'},
+            'additional_info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'address': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['userprofiles.DeliveryAddress']", 'null': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'employee': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Employee']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'CR'", 'max_length': '2'}),
+            'total_amount': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Unit']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'order.orderitem': {
+            'Meta': {'object_name': 'OrderItem'},
+            'cart': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['menu.Item']"}),
+            'old_price': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'order': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['order.Order']"})
+        },
+        'restaurant.communication': {
+            'Meta': {'object_name': 'Communication'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'restaurant.currency': {
+            'Meta': {'object_name': 'Currency'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'restaurant.deliverytype': {
+            'Meta': {'object_name': 'DeliveryType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'price': ('django.db.models.fields.FloatField', [], {})
+        },
+        'restaurant.employee': {
+            'Meta': {'object_name': 'Employee'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'start_date': ('django.db.models.fields.DateField', [], {})
+        },
+        'restaurant.partnerpackage': {
+            'Meta': {'object_name': 'PartnerPackage'},
+            'details': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'restaurant.paymentmethod': {
+            'Meta': {'object_name': 'PaymentMethod'},
+            'details': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'restaurant.unit': {
+            'Meta': {'object_name': 'Unit'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'admin_users': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'communication': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['restaurant.Communication']", 'symmetrical': 'False'}),
+            'contact_person': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'units_using_this'", 'to': "orm['restaurant.Currency']"}),
+            'delivery_range': ('django.db.models.fields.FloatField', [], {}),
+            'delivery_time': ('django.db.models.fields.IntegerField', [], {}),
+            'delivery_time_user': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'delivery_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.DeliveryType']"}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'employee': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Employee']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'logo_path': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {}),
+            'minimum_ord_val': ('django.db.models.fields.IntegerField', [], {}),
+            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'overall_discount': ('django.db.models.fields.FloatField', [], {}),
+            'package': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.PartnerPackage']"}),
+            'payment_method': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['restaurant.PaymentMethod']", 'symmetrical': 'False'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'})
+        },
+        'userprofiles.deliveryaddress': {
+            'Meta': {'ordering': "['-primary']", 'object_name': 'DeliveryAddress'},
+            'additional_info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'ap_number': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'block': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'default': "'Timisoara'", 'max_length': '50'}),
+            'entrance': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'floor': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'geolocated_address': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'geolocation_error': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'perform_geolocation': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'primary': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['order']
--- order/tests.py
+++ order/tests.py
@@ -14,33 +14,33 @@ class OrderTest(TestCase):
     self.unit = Unit.objects.get(pk=1)
 
   def test_abandoned_empty_removal(self):
-    old = Order.objects.create(user=self.user, unit=self.unit)
+    old = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     old.creation_date = datetime(2010, 9, 24)
     old.save()
-    self.failUnlessEqual(26, Order.objects.count())
+    self.failUnlessEqual(30, Order.objects.count())
     self.failUnlessEqual(0.0, old.total_amount)
     old.is_abandoned()
-    self.failUnlessEqual(25, Order.objects.count())
+    self.failUnlessEqual(29, Order.objects.count())
 
   def test_abandoned_nonempty_status(self):
-    old = Order.objects.create(user=self.user, unit=self.unit)
+    old = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     old.creation_date = datetime(2010, 9, 24)
     old.save()
-    self.failUnlessEqual(26, Order.objects.count())
+    self.failUnlessEqual(30, Order.objects.count())
     OrderItem.objects.create(order=old, item=Item.objects.get(pk=1))
     self.failUnlessEqual(1.23, old.total_amount)
     old.is_abandoned()
-    self.failUnlessEqual(26, Order.objects.count())
+    self.failUnlessEqual(30, Order.objects.count())
     self.failUnlessEqual('AB', old.status)
 
   def test_total_amount(self):
-    ord = Order.objects.create(user=self.user, unit=self.unit)
+    ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=1))
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2))
     self.failUnlessEqual(11.23, ord.total_amount)
 
   def test_cart_subtotal(self):
-    ord = Order.objects.create(user=self.user, unit=self.unit)
+    ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=1), cart="1")
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2), cart="1")
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2), cart="2")
@@ -49,7 +49,7 @@ class OrderTest(TestCase):
     self.failUnlessEqual(10, ord.get_cart_subtotal("2"))
 
   def test_get_carts(self):
-    ord = Order.objects.create(user=self.user, unit=self.unit)
+    ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     oi1 = OrderItem.objects.create(order=ord, item=Item.objects.get(pk=1), cart="1")
     oi2 = OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2), cart="1")
     oi3 = OrderItem.objects.create(order=ord, item=Item.objects.get(pk=3), cart="2")
@@ -58,26 +58,27 @@ class OrderTest(TestCase):
     self.failUnlessEqual(oi3, ord.get_carts()['2'][0])
 
   def test_clone(self):
-    ord1 = Order.objects.create(user=self.user, unit=self.unit)
+    ord1 = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     OrderItem.objects.create(order=ord1, item=Item.objects.get(pk=1), cart="1")
     OrderItem.objects.create(order=ord1, item=Item.objects.get(pk=2), cart="1")
     OrderItem.objects.create(order=ord1, item=Item.objects.get(pk=3), cart="2")
     ord2 = ord1.clone()
     delta = datetime.now() - ord2.creation_date
     self.assertTrue(delta.seconds < 5)
+    self.failUnlessEqual(ord1.employee, ord2.employee)
     self.failUnlessEqual('CR', ord2.status)
     self.failUnlessEqual(3, ord2.orderitem_set.count())
     self.failUnlessEqual(12.23, ord2.total_amount)
   
   def test_mark_delivered(self):
-      ord = Order.objects.create(user=self.user, unit=self.unit)
+      ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
       self.client.login(username='rif', password='test')
       r = self.client.get(reverse('order:restaurant_detail', args=[ord.id]))
       self.failUnlessEqual(200, r.status_code)
       #self.failUnlessEqual(u'RV', ord.status)
   
   def test_mark_delivered(self):
-      ord = Order.objects.create(user=self.user, unit=self.unit)
+      ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
       self.client.login(username='rif', password='test')
       r = self.client.get(reverse('order:restaurant_deliver', args=[ord.id]))
       self.failUnlessEqual(200, r.status_code)
@@ -85,7 +86,7 @@ class OrderTest(TestCase):
       #self.failUnlessEqual(u'DL', ord.status)
   
   def test_restricted_views(self):
-      ord = Order.objects.create(user=self.user, unit=self.unit)
+      ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
       self.client.login(username='rif', password='test')
       r = self.client.get(reverse('order:restaurant_detail', args=[ord.id]))
       self.failUnlessEqual(200, r.status_code)
