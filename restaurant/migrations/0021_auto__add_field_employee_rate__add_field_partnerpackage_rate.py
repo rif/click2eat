1983800f74dcf4a135e66e9966341088e4dac47f
Radu Ioan Fericean <fericean@gmail.com>
2010-10-12 20:33:52 +0300

    added rates for packages and employees

---
+++ restaurant/migrations/0021_auto__add_field_employee_rate__add_field_partnerpackage_rate.py
@@ -0,0 +1,113 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Employee.rate'
+        db.add_column('restaurant_employee', 'rate', self.gf('django.db.models.fields.FloatField')(default=1.0), keep_default=False)
+
+        # Adding field 'PartnerPackage.rate'
+        db.add_column('restaurant_partnerpackage', 'rate', self.gf('django.db.models.fields.FloatField')(default=1.0), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Employee.rate'
+        db.delete_column('restaurant_employee', 'rate')
+
+        # Deleting field 'PartnerPackage.rate'
+        db.delete_column('restaurant_partnerpackage', 'rate')
+
+
+    models = {
+        'restaurant.communication': {
+            'Meta': {'object_name': 'Communication'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'restaurant.currency': {
+            'Meta': {'object_name': 'Currency'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'restaurant.deliverytype': {
+            'Meta': {'object_name': 'DeliveryType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'price': ('django.db.models.fields.FloatField', [], {})
+        },
+        'restaurant.employee': {
+            'Meta': {'object_name': 'Employee'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'rate': ('django.db.models.fields.FloatField', [], {}),
+            'start_date': ('django.db.models.fields.DateField', [], {})
+        },
+        'restaurant.interval': {
+            'Meta': {'object_name': 'Interval'},
+            'end_hour': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'schedule': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Schedule']"}),
+            'start_hour': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'weekdays': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '13'})
+        },
+        'restaurant.partnerpackage': {
+            'Meta': {'object_name': 'PartnerPackage'},
+            'details': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'rate': ('django.db.models.fields.FloatField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'restaurant.paymentmethod': {
+            'Meta': {'object_name': 'PaymentMethod'},
+            'details': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'restaurant.schedule': {
+            'Meta': {'object_name': 'Schedule'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'unit': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['restaurant.Unit']", 'unique': 'True'})
+        },
+        'restaurant.unit': {
+            'Meta': {'object_name': 'Unit'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'added_date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'admin_users': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'communication': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['restaurant.Communication']", 'symmetrical': 'False'}),
+            'contact_person': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'units_using_this'", 'to': "orm['restaurant.Currency']"}),
+            'delivery_range': ('django.db.models.fields.FloatField', [], {}),
+            'delivery_time': ('django.db.models.fields.IntegerField', [], {}),
+            'delivery_time_user': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'delivery_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.DeliveryType']"}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'employee': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.Employee']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'logo_path': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {}),
+            'minimum_ord_val': ('django.db.models.fields.IntegerField', [], {}),
+            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'overall_discount': ('django.db.models.fields.FloatField', [], {}),
+            'package': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurant.PartnerPackage']"}),
+            'payment_method': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['restaurant.PaymentMethod']", 'symmetrical': 'False'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'})
+        }
+    }
+
+    complete_apps = ['restaurant']
