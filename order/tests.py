2d302fe0b5b23ebbd95e43d62da233a39af5fb75
Radu Ioan Fericean <radu@fericean.ro>
2010-10-13 18:26:46 +0300

    test

--- order/tests.py
+++ order/tests.py
@@ -30,7 +30,7 @@ class OrderTest(TestCase):
     old.save()
     self.failUnlessEqual(count + 1, Order.objects.count())
     OrderItem.objects.create(order=old, item=Item.objects.get(pk=1))
-    self.failUnlessEqual(1.23, old.total_amount)
+    self.failUnlessEqual(9.0, old.total_amount)
     old.is_abandoned()
     self.failUnlessEqual(count + 1, Order.objects.count())
     self.failUnlessEqual('AB', old.status)
@@ -39,15 +39,15 @@ class OrderTest(TestCase):
     ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=1))
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2))
-    self.failUnlessEqual(11.23, ord.total_amount)
+    self.failUnlessEqual(19.0, ord.total_amount)
 
   def test_cart_subtotal(self):
     ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=1), cart="1")
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2), cart="1")
     OrderItem.objects.create(order=ord, item=Item.objects.get(pk=2), cart="2")
-    self.failUnlessEqual(21.23, ord.total_amount)
-    self.failUnlessEqual(11.23, ord.get_cart_subtotal("1"))
+    self.failUnlessEqual(29.0, ord.total_amount)
+    self.failUnlessEqual(19.0, ord.get_cart_subtotal("1"))
     self.failUnlessEqual(10, ord.get_cart_subtotal("2"))
 
   def test_get_carts(self):
@@ -70,7 +70,7 @@ class OrderTest(TestCase):
     self.failUnlessEqual(ord1.employee, ord2.employee)
     self.failUnlessEqual('CR', ord2.status)
     self.failUnlessEqual(3, ord2.orderitem_set.count())
-    self.failUnlessEqual(12.23, ord2.total_amount)
+    self.failUnlessEqual(20.0, ord2.total_amount)
   
   def test_mark_delivered(self):
       ord = Order.objects.create(user=self.user, unit_id=self.unit.id, employee_id=self.unit.employee_id)
