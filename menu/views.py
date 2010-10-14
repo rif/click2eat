7f9b18991cd9cc8856ee486f7c6e3d8a8fe39daa
Radu Ioan Fericean <radu@fericean.ro>
2010-10-14 17:33:09 +0300

    search in tags

--- menu/views.py
+++ menu/views.py
@@ -8,6 +8,6 @@ def item_list(request):
     query = request.GET.get('q', '')
     results = Item.objects.all()
     if query:
-        results = results.filter(Q(internal_name__icontains=query) | Q(unit__name__icontains=query))
+        results = results.filter(Q(internal_name__icontains=query) | Q(tags__name__icontains=query) | Q(unit__name__icontains=query))
     f = ItemFilter(request.GET, queryset=results)
     return {'query': query, 'filter': f}
