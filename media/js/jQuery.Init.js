1c4ee487d799c90c357d8e5fbdc2d489cfdafbf1
Radu Fericean <radu.fericean@oce.com>
2010-11-13 15:36:01 +0200

    partial art drop five

--- media/js/jQuery.Init.js
+++ media/js/jQuery.Init.js
@@ -1,13 +1,5 @@
 $(function() {
-	$("#menu #menu-list li img").hover(
-		function (){
-			var src = $(this).attr("src").match(/[^\.]+/) + "-o.gif";
-			$(this).attr("src", src);
-		},
-
-		function (){
-			var src = $(this).attr("src").replace("-o", "");
-            $(this).attr("src", src);
-		}
-	);
+	$('.clearme').one("focus", function() {
+		$(this).val("");
+	});
 });
