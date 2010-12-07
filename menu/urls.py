83e6200330561aed96b26f7386080b171107a14a
Radu Ioan Fericean <radu@fericean.ro>
2010-12-07 21:51:33 +0200

    rss on the first page + bonus points content

--- menu/urls.py
+++ menu/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
                        url(r'^itemlist/$', views.item_list, name='item_list'),
                        url(r'^dailymenues/$', views.daily_menues, name='daily_menues'),
                        url(r'^dailymenu/(?P<menu_id>\d+)/$', views.daily_menu, name='daily_menu'),
-                       url(r'^feed/$', DailyMenuFeed()),
+                       url(r'^feed/$', DailyMenuFeed(), name='daily_feed'),
                        )
