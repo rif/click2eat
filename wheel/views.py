c359cb141cb32f54c90b454c167212121196e49b
Radu Ioan Fericean <radu@fericean.ro>
2010-12-13 09:50:09 +0200

    please login for wheel

--- wheel/views.py
+++ wheel/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.http import HttpResponse
 from datetime import date
 from menu.models import MenuOfTheDay
