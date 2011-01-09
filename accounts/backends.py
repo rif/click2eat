b4470e08d525b752f1750025574e85388ad55e7f
Radu Ioan Fericean <radu@fericean.ro>
2011-01-09 21:25:31 +0200

    added email login and focus for cart name

---
+++ accounts/backends.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.core.validators import email_re
+
+class EmailOrUsernameModelBackend(object):
+    def authenticate(self, username=None, password=None):
+        if email_re.search(username):
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+        try:
+            user = User.objects.get(**kwargs)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
