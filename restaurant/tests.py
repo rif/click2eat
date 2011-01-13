feb3936211bf5b5098ae3b9e1f941d63655e0afe
Radu Ioan Fericean <radu@fericean.ro>
2011-01-14 00:02:35 +0200

    fixed test after time conversion

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from datetime import datetime
+from datetime import datetime, time
 from django.contrib.auth.models import User
 from restaurant.models import Unit, Schedule, Interval
 
@@ -12,7 +12,7 @@ class UnitTest(TestCase):
     
     def test_schedule(self):
         s = Schedule(unit=self.unit)
-        i1 = Interval(schedule=s, weekdays="1,2", start_hour="10:00", end_hour="12:00")
+        i1 = Interval(schedule=s, weekdays="1,2", start_hour=time(10,0), end_hour=time(12,0))
         self.assertTrue(i1._is_open(datetime(2010,10,5,11,0)))
         self.assertFalse(i1._is_open(datetime(2010,10,5,12,1)))
         self.assertFalse(i1._is_open(datetime(2010,10,5,9,0)))
@@ -20,7 +20,7 @@ class UnitTest(TestCase):
         
     def test_after_midnight_schedule(self):
         s = Schedule(unit=self.unit)
-        i1 = Interval(schedule=s, weekdays="1,2,3,4,5,6,7", start_hour="10:00", end_hour="01:00")
+        i1 = Interval(schedule=s, weekdays="1,2,3,4,5,6,7", start_hour=time(10,0), end_hour=time(1,0))
         self.assertTrue(i1._is_open(datetime(2010,10,5,11,0)))
         self.assertTrue(i1._is_open(datetime(2010,10,5,23,15)))
         self.assertFalse(i1._is_open(datetime(2010,10,5,3,1)))
