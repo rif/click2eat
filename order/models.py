9ab498dbff8cf1060e917900f5fc730b209cb443
Radu Ioan Fericean <radu@fericean.ro>
2011-01-19 17:19:16 +0200

    added order delivery type

--- order/models.py
+++ order/models.py
@@ -14,6 +14,7 @@ class Order(models.Model):
     )
     user = models.ForeignKey(User, verbose_name=_('user'), editable=False)
     address = models.ForeignKey('userprofiles.DeliveryAddress', verbose_name=_('address'), null=True, blank=True)
+    delivery_type = models.ForeignKey('restaurant.DeliveryType', verbose_name=_('delivery type'), null=True, blank=True)
     creation_date = models.DateTimeField(_('creation date'), auto_now_add=True, editable=False)
     status = models.CharField(_('status'), max_length=2, choices=STATUS_CHOICES, default='CR')
     total_amount = models.FloatField(_('total amount'), default=0)
