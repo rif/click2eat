0299191c98ae6442061af64243bbab24bfdea38a
Radu Ioan Fericean <radu@fericean.ro>
2011-04-29 23:03:44 +0300

    motd started

--- menu/urls.py
+++ menu/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
                        url(r'^itemtaglist/(?P<tag>\w+)$', views.item_tag_list, name='item_tag_list'),
                        url(r'^menulist/$', views.menu_list, name='menu_list'),
                        url(r'^dailymenus/$', views.daily_menus, name='daily_menus'),
+                       url(r'^randommotd/$', views.random_motd, name='random_motd'),
                        url(r'^dailymenu/(?P<menu_id>\d+)/$', views.daily_menu, name='daily_menu'),
                        url(r'^feed/$', DailyMenuFeed(), name='daily_feed'),
                        )
+
