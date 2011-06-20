ef6262a7e8f58f9bf226ef9d24714ecab49cea37
Radu Ioan Fericean <rif@love.ro>
2011-06-20 20:12:57 +0300

    more more more

--- menu/urls.py
+++ menu/urls.py
@@ -5,7 +5,7 @@ from menu.feeds import DailyMenuFeed
 
 urlpatterns = patterns('',
                        url(r'^itemlist/$', views.item_list, name='item_list'),
-                       url(r'^itemtaglist/(?P<tag>\w+)$', views.item_tag_list, name='item_tag_list'),
+                       url(r'^itemtaglist/(?P<tag>\w+-?\w+)$', views.item_tag_list, name='item_tag_list'),
                        url(r'^menulist/$', views.menu_list, name='menu_list'),
                        url(r'^dailymenus/$', views.daily_menus, name='daily_menus'),
                        url(r'^randommotd/$', views.random_motd, name='random_motd'),
