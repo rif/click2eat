79453ff169f531a64ac81d5d03435fe1ed548506
Radu Ioan Fericean <rif@love.ro>
2011-08-16 19:03:12 +0300

    login

--- mobile/urls.py
+++ mobile/urls.py
@@ -4,5 +4,9 @@ from mobile import views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', TemplateView.as_view(template_name="mobile/home.html")),
+                       url(r'^$', TemplateView.as_view(template_name='mobile/home.html'), name='home'),
+                       url(r'^units/$', views.units, name='units'),
+                       url(r'^menu/(?P<unit_id>\d+)/$', views.menu, name='menu'),
+                       url(r'^accounts/login/$','django.contrib.auth.views.login', {'template_name':'mobile/login.html'},name='login'),
+                       url(r'^accounts/logout/$','django.contrib.auth.views.logout',{'template_name':'mobile/logout.html'}, name='logout'),
                        )
