33bc41b3592f2c7d21a70c04a0f13a777512e354
Radu Ioan Fericean <radu@fericean.ro>
2011-08-16 19:13:36 +0300

    login action

--- mobile/views.py
+++ mobile/views.py
@@ -3,13 +3,13 @@ from annoying.decorators import render_to
 from restaurant.models import Unit
 from django.shortcuts import get_object_or_404
 
-@login_required
+#@login_required(login_url='/mobile/accounts/login/')
 @render_to('mobile/units.html')
 def units(request):
 	units = Unit.objects.all()
 	return dict(units = units)
 
-@login_required
+#@login_required(login_url='/mobile/accounts/login/')
 @render_to('mobile/menu.html')
 def menu(request, unit_id):
 	unit = get_object_or_404(Unit, pk=unit_id)
