ac7f44220e9f05542fcf8ca5cde1541bef51a8d7
Radu Ioan Fericean <rif@love.ro>
2011-08-25 18:08:21 +0300

    wheel of fortune

--- wheel/urls.py
+++ wheel/urls.py
@@ -3,6 +3,5 @@ from wheel import views
 
 
 urlpatterns = patterns('django.views.generic.simple',
-                       url(r'^$', 'direct_to_template', {'template': 'wheel/fortune.html'}, name='fortune'),
                        url(r'^sample/$', views.fortune_ajax, name='fortune_ajax'),
                        )
