63c40ded442a87d787836b25538cfc09485a837b
Radu Ioan Fericean <rif@love.ro>
2011-08-30 11:51:44 +0300

    do not show link at fortune wheel if user is not logged in

--- wheel/views.py
+++ wheel/views.py
@@ -10,7 +10,9 @@ from menu.models import Item
 @render_to('wheel/random_item.html')
 def fortune_ajax(request):
     item = Item.objects.select_related('item_group').order_by('?')[0]
-    command_link = '<a onclick="addItem(\'%(href)s\', \'%(redir)s\'); return false;" href="#">%(order_now)s</a>' % \
+    command_link = ''
+    if request.user.is_authenticated:
+    	command_link = '<a onclick="addItem(\'%(href)s\', \'%(redir)s\'); return false;" href="#">%(order_now)s</a>' % \
             {'redir': reverse("restaurant:detail", args=[item.item_group.unit_id]),
              'href': reverse("order:add_item", args=[item.id, request.user.username]),
              'order_now': _('Order this Item')}
