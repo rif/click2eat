77430b456c761d4ef4e24dec3ce4d82472c86cfc
Radu Ioan Fericean <rif@love.ro>
2011-09-06 14:11:47 +0300

    mobile item search

--- mobile/urls.py
+++ mobile/urls.py
@@ -5,8 +5,10 @@ from mobile import views
 
 urlpatterns = patterns('',
                        url(r'^$', TemplateView.as_view(template_name='mobile/home.html'), name='home'),
-                       url(r'^units/$', views.units, name='units'),
-                       url(r'^menu/(?P<unit_id>\d+)/$', views.menu, name='menu'),
                        url(r'^accounts/login/$','django.contrib.auth.views.login', {'template_name':'mobile/login.html'},name='login'),
                        url(r'^accounts/logout/$','django.contrib.auth.views.logout',{'template_name':'mobile/logout.html'}, name='logout'),
+                       url(r'^units/$', views.units, name='units'),
+                       url(r'^menu/(?P<unit_id>\d+)/$', views.menu, name='menu'),
+                       url(r'^search/$', views.search, name='search'),
+                       url(r'^itemdetail/(?P<item_id>\d+)/$', views.item_detail, name='item-detail'),
                        )
--- mobile/views.py
+++ mobile/views.py
@@ -1,16 +1,27 @@
 from django.contrib.auth.decorators import login_required
 from annoying.decorators import render_to
 from restaurant.models import Unit
+from menu.models import Item
 from django.shortcuts import get_object_or_404
 
 #@login_required(login_url='/mobile/accounts/login/')
 @render_to('mobile/units.html')
 def units(request):
 	units = Unit.objects.all()
-	return dict(units = units)
+	return locals()
 
 #@login_required(login_url='/mobile/accounts/login/')
 @render_to('mobile/menu.html')
 def menu(request, unit_id):
 	unit = get_object_or_404(Unit, pk=unit_id)
-	return dict(unit = unit)
+	return locals()
+
+@render_to('mobile/search.html')
+def search(request):
+        items = Item.objects.select_related('tags')
+        return locals()
+
+@render_to('mobile/item_detail.html')
+def item_detail(request, item_id):
+        item = get_object_or_404(Item, pk=item_id)
+        return locals()
