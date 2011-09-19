fe59cf73c2221e3dda84e9ef9d65ae1abcd30159
Radu Ioan Fericean <radu@fericean.ro>
2011-09-19 12:37:10 +0300

    separate sentry still not working

--- sentry.conf.py
+++ sentry.conf.py
@@ -1,6 +1,13 @@
 from sentry.conf.defaults import *
 from sentry.conf.server import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/rif/sentry.db',
+     }
+}
+
 SENTRY_WEB_HOST = '0.0.0.0'
 SENTRY_WEB_PORT = 9000
 
