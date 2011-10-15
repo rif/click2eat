0cce41de23464dd1735f97b51476b0252d5e3ad4
Radu Fericean <radu@fericean.ro>
2011-10-15 23:28:06 +0300

    messages visible

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 #from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
+from django.core.cache import cache
 from django.views.generic.create_update import update_object, delete_object
 from django.views.generic.list_detail import object_detail
 from django.shortcuts import redirect
@@ -20,9 +21,10 @@ from friends.models import JoinInvitation
 from annoying.decorators import render_to
 from django.core.urlresolvers import reverse
 
+
 @login_required
 def profile_detail(request, username):
-    orders = request.user.order_set.exclude(hidden=True).exclude(status='CR')[:20]
+    orders = request.user.order_set.exclude(hidden=True)[:20]
     extra_context = {'orders': orders}
     dict = {'username': username, 'extra_context': extra_context}
     if username != request.user.username:
@@ -30,7 +32,9 @@ def profile_detail(request, username):
     return views.profile_detail(request, **dict)
 
 @login_required
-def create(request):
+def create(request, from_order=0):
+    if from_order:
+        cache.set('back_to_order', from_order, 300) #hold the thought fro 5 minutes
     if request.method == 'POST':
         form = DeliveryAddressForm(request.POST)
         if form.is_valid():
@@ -93,8 +97,7 @@ def limited_update_object(*args, **kwargs):
     request = args[0]
     object_id = kwargs['object_id']
     addr = get_object_or_404(DeliveryAddress, pk=object_id)
-    if addr.user_id != request.user.id:
-        raise PermissionDenied()
+    if addr.user_id != request.user.id: raise PermissionDenied()
     return update_object(*args, **kwargs)
 
 @login_required
@@ -103,8 +106,7 @@ def limited_delete_object(*args, **kwargs):
     model = kwargs['model']
     object_id = kwargs['object_id']
     addr = get_object_or_404(model, pk=object_id)
-    if addr.user_id != request.user.id:
-        raise PermissionDenied()
+    if addr.user_id != request.user.id: raise PermissionDenied()
     return delete_object(*args, post_delete_redirect=reverse('profiles_profile_detail', kwargs={'username':request.user.username}), **kwargs)
 
 @login_required
@@ -113,6 +115,5 @@ def limited_object_detail(*args, **kwargs):
     queryset = kwargs['queryset']
     object_id = kwargs['object_id']
     addr = queryset.get(pk=object_id)
-    if addr.user_id != request.user.id:
-        raise PermissionDenied()
+    if addr.user_id != request.user.id: raise PermissionDenied()
     return object_detail(*args, **kwargs)
