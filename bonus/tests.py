8a48ab55b67aeb904aaec5396c61cf6431cc4f60
Radu Ioan Fericean <radu@fericean.ro>
2011-11-02 01:31:49 +0200

    test working after refactoring

--- bonus/tests.py
+++ bonus/tests.py
@@ -1,14 +1,9 @@
 from django.test import TestCase
-from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from django.db.models import Sum
-from bonus.models import BonusTransaction
-from userprofiles.models import UserProfile
 
 class SimpleTest(TestCase):
-    fixtures = ['users.json', 'userprofiles.json', 'bonus.json']
+    fixtures = ['users.json', 'userprofiles.json', 'bonus.json', 'order.json']
 
     def test_total_sum(self):
         user = User.objects.get(pk=2)
-        print BonusTransaction.objects.all().values()
         self.assertEqual(6.0, user.get_profile().get_current_bonus())
