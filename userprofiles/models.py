579d7dd889188d716da8f460eb2e34f913ed3d42
Radu Fericean <radu@fericean.ro>
2011-12-08 15:51:45 +0200

    added shc

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -37,7 +37,7 @@ class DeliveryAddress(models.Model):
         return self.get_full_address()
 
     def get_full_address(self):
-        address = _('%(street)s %(number)s, %(city)s') % {'street': self.street, 'number': self.number, 'city': self.city}
+        address = _('%(st)s %(nb)s, %(city)s') % {'street': self.street, 'number': self.number, 'city': self.city}
         if self.block:
             address += _(', bl: %s') % self.block
         if self.entrance:
