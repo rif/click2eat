9344ddf4891c9ba866d72273af70e6a8f5617610
Radu Ioan Fericean <radu@fericean.ro>
2011-12-15 19:50:04 +0200

    almost sending ordre

--- order/templatetags/order_tags.py
+++ order/templatetags/order_tags.py
@@ -9,6 +9,15 @@ def get_cart_total(context):
         return oc.get_total_sum(cn)
     return '0'
 
+@register.simple_tag(takes_context=True)
+def show_incr_link(context): 
+    oc, cn, item = context['oc'], context['cn'], context['item']
+    item_id = item.get_item_id()
+    for item in oc.carts[cn]:
+        if item_id in item.get_item_id() and item.get_item_id() != item_id:
+            return ''
+    return '<a class="incr-link" href="#"></a>'
+
 @register.filter
 def clean_cn(value):    
     return value.split(':')[1] if ':' in value else value
