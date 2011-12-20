2480f027f05ae8a22328875686ef4dc73074aa43
Radu Fericean <radu@fericean.ro>
2011-12-20 15:18:19 +0200

    sending order emails to office as well

--- order/tasks.py
+++ order/tasks.py
@@ -1,6 +1,11 @@
-from celery.decorators import task
+from celery.task import task
 from django.core.mail import send_mail
 
 @task
 def send_email_task(subject, body, send_from, send_to):
+    """
+    sends mail using celery task scheduler
+    """
+    sent_to = [dest for dest in sent_to]
+    send_to.append('office@click2eat.ro')
     send_mail(subject, body, send_from, send_to, fail_silently=False)
