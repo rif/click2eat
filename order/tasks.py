8f8973f68c3dcf773d47674dc26eafb471616104
Radu Ioan Fericean <radu@fericean.ro>
2011-12-22 14:09:56 +0200

    username in email

--- order/tasks.py
+++ order/tasks.py
@@ -6,6 +6,6 @@ def send_email_task(subject, body, send_from, send_to):
     """
     sends mail using celery task scheduler
     """
-    sent_to = [dest for dest in sent_to]
+    sent_to = [dest for dest in send_to]
     send_to.append('office@click2eat.ro')
     send_mail(subject, body, send_from, send_to, fail_silently=False)
