f8936354619c25b0562d6448096fb29b766ce01f
Radu Fericean <radu@fericean.ro>
2012-01-08 18:51:28 +0200

    tests are passing again

--- mobile/tests.py
+++ mobile/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from restaurant.models import Unit
+from order.shopping_service import OrderCarts
+from time import time
+
+def uid(id, topid=0):
+    r = '%s!%s-0' %(int(time()*1000), id)
+    if topid: r = r + '_' + topid
+    return r
 
 class MobileTest(TestCase):
     fixtures = ['restaurant.json', 'menu.json', 'users.json']
@@ -8,49 +15,52 @@ class MobileTest(TestCase):
     def test_cart_total(self):
         login_successful = self.client.login(username='rif', password='test')
         self.assertTrue(login_successful)
-        r = self.client.get(reverse('order:shop', args=['rif', '1']))
+        r = self.client.get(reverse('order:shop', args=[1, "rif", uid('1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals('{"price": 10.0, "total": 10.0, "subtotal": 10.0, "id": "1", "name": "Tocanita de puioc"}', r.content)
+        self.assertTrue('<span id="order-total">12.0</span>' in r.content)
 
     def test_session_item(self):
         self.client.login(username='rif', password='test')
-        r = self.client.get(reverse('order:shop', args=['rif', '1']))
+        r = self.client.get(reverse('order:shop', args=[1, "rif", uid('1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals( {u'1': [1, 10.0, u'Tocanita de puioc']}, self.client.session['1:rif'])
+        oc = OrderCarts(self.client.session, '1')
+        self.assertEqual(12, oc.get_total_sum())
 
     def test_session_motd(self):
         self.client.login(username='rif', password='test')
-        r = self.client.get(reverse('order:shop', args=['rif', 'm1']))
+        r = self.client.get(reverse('order:shop', args=[1, "rif", uid('m1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals({u'm1': [1, 10.0, u'Meniu complet cu 10 lei']}, self.client.session['1:rif'])
+        oc = OrderCarts(self.client.session, '1')
+        self.assertEqual(10, oc.get_total_sum())
 
     def test_session_top(self):
         self.client.login(username='rif', password='test')
-        self.client.get(reverse('order:shop', args=['rif', '1']))
-        r = self.client.get(reverse('order:shop', args=['rif', '1_1']))
+        self.client.get(reverse('order:shop', args=[1, "rif", uid('1')]))
+        r = self.client.get(reverse('order:shop', args=["1", "rif", uid('1','1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals({u'1': [1, 10.0, u'Tocanita de puioc'], u'1_1': [1, 1.0, u'Cascaval']}, self.client.session['1:rif'])
+        oc = OrderCarts(self.client.session, '1')
+        self.assertEqual(13, oc.get_total_sum())
 
     def test_session_first_top(self):
         self.client.login(username='rif', password='test')
-        r = self.client.get(reverse('order:shop', args=['rif', '1_1']))
+        r = self.client.get(reverse('order:shop', args=[1, "rif", uid('1','1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals('{"error": "2e62"}', r.content)
+        self.assertTrue('<span id="order-total"></span>' in r.content)
 
     def test_session_top_without_item(self):
         self.client.login(username='rif', password='test')
-        r = self.client.get(reverse('order:shop', args=['rif', '2']))
-        r = self.client.get(reverse('order:shop', args=['rif', '1_1']))
+        r = self.client.get(reverse('order:shop', args=[1, "rif", uid('2')]))
+        r = self.client.get(reverse('order:shop', args=[1, "rif", uid('1','1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals('{"error": "2e6z"}', r.content)
+        self.assertTrue('<span id="order-total">11.99</span>' in r.content)
 
     def test_min_order(self):
         u = Unit.objects.get(id=1)
         u.minimum_ord_val = 100
         u.save()
         self.client.login(username='rif', password='test')
-        self.client.get(reverse('order:shop', args=['rif', '1']))
-        r = self.client.get(reverse('order:send-order', args=['1']))
+        self.client.get(reverse('order:shop', args=[1, "rif", uid('1')]))
+        r = self.client.get(reverse('mobile:send-order', args=['1']))
         self.assertEqual(200, r.status_code)
         self.assertTrue(r.content in ['{"error": "2e65"}','{"error": "2e61"}']) # maybe closed
 
@@ -59,20 +69,22 @@ class MobileTest(TestCase):
         u.minimum_ord_val = 100
         u.save()
         self.client.login(username='rif', password='test')
-        self.client.get(reverse('order:shop', args=['rif', '1']))
-        self.client.get(reverse('order:shop', args=['rif', '1_1']))
-        r = self.client.get(reverse('order:decr-item', args=['rif', '1', '1']))
+        self.client.get(reverse('order:shop', args=[1, "rif", uid('1')]))
+        self.client.get(reverse('order:shop', args=[1, "rif", uid('1','1')]))
+        r = self.client.get(reverse('order:decr-item', args=[1, 'rif', uid('1')]))
         self.assertEqual(200, r.status_code)
-        self.assertTrue('1:rif' not in self.client.session)
+        oc = OrderCarts(self.client.session, '1')
+        self.assertEqual(13, oc.get_total_sum())
 
     def test_incr_decr(self):
         u = Unit.objects.get(id=1)
         u.minimum_ord_val = 100
         u.save()
         self.client.login(username='rif', password='test')
-        self.client.get(reverse('order:shop', args=['rif', '1']))
-        r = self.client.get(reverse('order:incr-item', args=['rif', '1', '1']))
-        self.assertEquals({u'1': [2, 10.0, u'Tocanita de puioc']}, self.client.session['1:rif'])
-        r = self.client.get(reverse('order:decr-item', args=['rif', '1', '1']))
+        oc = OrderCarts(self.client.session, '1')
+        self.client.get(reverse('order:shop', args=[1, "rif", uid('1')]))
+        r = self.client.get(reverse('order:incr-item', args=[1, "rif", uid('1')]))
+        self.assertEqual(0, oc.get_total_sum())
+        r = self.client.get(reverse('order:decr-item', args=[1, "rif", uid('1')]))
         self.assertEqual(200, r.status_code)
-        self.assertEquals({u'1': [1, 10.0, u'Tocanita de puioc']}, self.client.session['1:rif'])
+        self.assertEqual(0, oc.get_total_sum())
