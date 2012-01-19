a6c92dead86c9cec9684c3887c8d15d40f1953d5
Radu Fericean <radu@fericean.ro>
2012-01-19 13:32:51 +0200

    pagination and redis query

--- rsv/views.py
+++ rsv/views.py
@@ -1,32 +1,39 @@
-from django.views.generic import TemplateView
 from django.core.exceptions import PermissionDenied
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.decorators import login_required
+from annoying.decorators import render_to
 from django.contrib.auth.models import User
-from redis_sessions import session 
+from redis_sessions import session
+from rsv.forms import QueryForm
 import redis
 import base64
 import cPickle as pickle
 
-class IndexView(TemplateView):
-    template_name = "rsv/index.html"
+@login_required
+@render_to('rsv/index.html')
+def index(request):
+    if not request.user.is_staff: raise PermissionDenied()
 
-    def get_context_data(self, **kwargs):
-        if not self.request.user.is_staff: raise PermissionDenied()
-        context = super(IndexView, self).get_context_data(**kwargs)                
+    r = redis.StrictRedis(host=getattr(settings, 'SESSION_REDIS_HOST', 'localhost'),
+        port=getattr(settings, 'SESSION_REDIS_PORT', 6379),
+        db=getattr(settings, 'SESSION_REDIS_DB', 0),
+        password=getattr(settings, 'SESSION_REDIS_PASSWORD', None)
+        )
+    ss = session.SessionStore()
+    sessions = []
+    query = '*'            
+    if request.method == 'POST':
+        form = QueryForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query'] + '*'
+    else:
+        form = QueryForm() # An unbound form    
+        
+    for session_id in r.keys(query):
+        d = ss.decode(r.get(session_id))
+        if '_auth_user_id' in d:
+            a = {'key': session_id, 'user': User.objects.get(pk=d['_auth_user_id'])}
+            sessions.append(a)
 
-        r = redis.StrictRedis(host=getattr(settings, 'SESSION_REDIS_HOST', 'localhost'),
-            port=getattr(settings, 'SESSION_REDIS_PORT', 6379),
-            db=getattr(settings, 'SESSION_REDIS_DB', 0),
-            password=getattr(settings, 'SESSION_REDIS_PASSWORD', None)
-            )
-        ss = session.SessionStore()
-        sessions = []
-        for session_id in r.keys('*'):
-            d = ss.decode(r.get(session_id))
-            if '_auth_user_id' in d:
-                a = {'key': session_id, 'user': User.objects.get(pk=d['_auth_user_id'])}
-                sessions.append(a)
-
-        context['sessions'] = sessions
-        return context
+    return {'form': form, 'sessions': sessions}
+    
