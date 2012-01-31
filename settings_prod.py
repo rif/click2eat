02d3cc94040076a4731b337c1733b8e1debf060a
Radu Ioan Fericean <radu@fericean.ro>
2012-01-31 19:40:28 +0200

    modified logger

--- settings_prod.py
+++ settings_prod.py
@@ -21,20 +21,36 @@ SENTRY_KEY = '2d56cabc3073637ff33471e2ec84de27f99180f6'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
     'handlers': {
         'sentry': {
-            'level': 'DEBUG',
+            'level': 'ERROR',
             'class': 'raven.contrib.django.handlers.SentryHandler',
         },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         }
     },
     'loggers': {
-        '()': {
-            'level': 'WARNING',
-            'handlers': ['sentry'],
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
         },
         'sentry.errors': {
             'level': 'DEBUG',
