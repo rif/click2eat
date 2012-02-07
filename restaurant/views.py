33af56000dcd7943a4879c1aa349674f93b7a227
Radu Ioan Fericean <radu@fericean.ro>
2012-02-07 13:16:14 +0200

    even better time range for invoices

--- restaurant/views.py
+++ restaurant/views.py
@@ -8,7 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db.models import Sum, Avg, Count
 from django.db.models import Q
 from annoying.decorators import render_to
-from datetime import datetime
+from datetime import date, timedelta
 from restaurant.models import Unit, PartnerPackage
 from order.models import Order, Rating
 from userprofiles.models import BonusTransaction
@@ -79,21 +79,21 @@ def package_history(request, unit_id):
 def invoice(request, unit_id):
     unit = get_object_or_404(Unit, pk=unit_id)
     __is_restaurant_administrator(request, unit)
-    end = now = datetime.now()
-    start = now.replace(day=1)
+    end = today = date.today()
+    start = today.replace(day=1)
     if request.method == 'POST':
         form = InvoiceForm(request.POST)
         if form.is_valid():
             start = form.cleaned_data['start_date']
-            end = form.cleaned_data['end_date']
-            end = datetime.combine(end, now.time()) # to include the orders made today
+            end = form.cleaned_data['end_date']            
     else:
-        form = InvoiceForm(initial={'start_date':start, 'end_date':now})
-    orders = Order.objects.filter(unit=unit).filter(creation_date__range=(start, end)).filter(status__in=['ST', 'RV', 'DL'])
+        form = InvoiceForm(initial={'start_date':start, 'end_date':today})
+    interval_end = end + timedelta(1) # the next day 0:00
+    orders = Order.objects.filter(unit=unit).filter(creation_date__range=(start, interval_end)).filter(status__in=['ST', 'RV', 'DL'])
     total_sum = orders.aggregate(Sum('total_amount'))['total_amount__sum'] or 0
-    bonuses = BonusTransaction.objects.filter(order__unit=unit).filter(order__creation_date__range=(start, end)).filter(amount__lt=0)
+    bonuses = BonusTransaction.objects.filter(order__unit=unit).filter(order__creation_date__range=(start, interval_end)).filter(amount__lt=0)
     total_bonus = bonuses.aggregate(Sum('amount'))['amount__sum'] or 0    
-    package = unit.partnerpackage_set.filter(start_date__lte=start).filter(Q(end_date__gte=end) | (Q(end_date=None) & Q(current=True)))
+    package = unit.partnerpackage_set.filter(start_date__lte=start).filter(Q(end_date__gte=interval_end) | (Q(end_date=None) & Q(current=True)))
     if package.exists() and package.count() == 1:
         package = package[0]
     else:
